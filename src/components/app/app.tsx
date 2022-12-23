064e18404c718d36b71e31569c85fb8dd1d75874
Irina Ryazanskaya <ryazanskaya.irina@yandex.ru>
2022-12-23 22:15:17 +0100

    feat: получить данные ингридиентов

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from 'react';
+
+import { getIngridients } from '../../api';
+
 import styles from './app.module.css';
 
 function App() {
+  const [ingridients, setIngridients] = useState();
+
+  useEffect(() => {
+    getIngridients()
+      .then((data) => {
+        setIngridients(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }, []);
+
+  console.log(ingridients);
+
   return (
     <div className={styles.app}>
-      
+    
     </div>
   );
 }
